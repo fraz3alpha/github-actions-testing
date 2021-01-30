8975873a416436a5ea344a1369a62d994d560b6e
fraz3alpha <andrewtaylor2@gmail.com>
2021-01-30 15:57:56 +0000

    Print tool versions

--- website/build.sh
+++ website/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "This is the bulid script for the website, running in `pwd`"
+
+which python && python -V || echo "No python"
+which npm && npm version || echo "No npm"
+which node && node -v || echo "No node"
